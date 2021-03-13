05ed6947604c45f97bd493ee10e5803dd74b5f6b
John Crawford <john.crawford@uci.edu>
2021-03-13 09:10:36 -0800

    update build script

--- docs/scripts/build-status.js
+++ docs/scripts/build-status.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const { request } = require('@octokit/request')
 
+const REPO_OWNER = 'embodied'
+const REPO_NAME = 'active-space'
+
 async function getBuildStatus() {
   const result = await request('GET /repos/{owner}/{repo}/pages/builds/latest', {
     headers: {
       authorization: `token ${process.env.JEKYLL_GITHUB_TOKEN}`,
     },
-    owner: 'embodied',
-    repo: 'active-space',
+    owner: REPO_OWNER,
+    repo: REPO_NAME,
   })
   console.log(`build status: ${result.data.status}`)
 }
